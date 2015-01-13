f43a89730f4966c8f6657c7f6795af790cc0ab71
Alexis Kofman <alexis.kofman@gmail.com>
2015-01-13 13:59:25 +0100

    add a provider to init default recognizer options

--- src/gestures.js
+++ src/gestures.js
@@ -16,58 +16,73 @@ angular.module('angular-gestures', []);
  * or any other of the "hm-event" listed underneath.
  */
 var HGESTURES = {
-  hmDoubleTap : 'doubletap',
-  hmDragstart : 'dragstart',
-  hmDrag : 'drag',
-  hmDragUp : 'dragup',
-  hmDragDown : 'dragdown',
-  hmDragLeft : 'dragleft',
-  hmDragRight : 'dragright',
-  hmDragend : 'dragend',
-  hmHold : 'press',
-  hmPinch : 'pinch',
-  hmPinchIn : 'pinchin',
-  hmPinchOut : 'pinchout',
-  hmPress : 'press',
-  hmRelease : 'release',
-  hmRotate : 'rotate',
-  hmSwipe : 'swipe',
-  hmSwipeUp : 'swipeup',
-  hmSwipeDown : 'swipedown',
-  hmSwipeLeft : 'swipeleft',
-  hmSwipeRight : 'swiperight',
-  hmTap : 'tap',
-  hmTouch : 'touch',
-  hmTransformstart : 'transformstart',
-  hmTransform : 'transform',
-  hmTransformend : 'transformend'
-};
+    hmDoubleTap: 'doubletap',
+    hmDragstart: 'dragstart',
+    hmDrag: 'drag',
+    hmDragUp: 'dragup',
+    hmDragDown: 'dragdown',
+    hmDragLeft: 'dragleft',
+    hmDragRight: 'dragright',
+    hmDragend: 'dragend',
+    hmHold: 'press',
+    hmPinch: 'pinch',
+    hmPinchIn: 'pinchin',
+    hmPinchOut: 'pinchout',
+    hmPress: 'press',
+    hmRelease: 'release',
+    hmRotate: 'rotate',
+    hmSwipe: 'swipe',
+    hmSwipeUp: 'swipeup',
+    hmSwipeDown: 'swipedown',
+    hmSwipeLeft: 'swipeleft',
+    hmSwipeRight: 'swiperight',
+    hmTap: 'tap',
+    hmTouch: 'touch',
+    hmTransformstart: 'transformstart',
+    hmTransform: 'transform',
+    hmTransformend: 'transformend'
+  };
 
 var VERBOSE = true;
 
 angular.forEach(HGESTURES, function(eventName, directiveName) {
-  angular.module('angular-gestures').directive( directiveName,
-    ['$parse', '$log', '$timeout', function($parse, $log, $timeout) {
-      return function(scope, element, attr) {
-        var hammertime, handler;
-        attr.$observe(directiveName, function(value) {
-          var fn = $parse(value);
-          var opts = $parse(attr[directiveName + 'Opts'])
-          (scope, {});
-          hammertime = new Hammer(element[0], opts);
-          handler = function(event) {
-            if (VERBOSE) {
-              $log.debug('angular-gestures: ', eventName, event);
-            }
-            $timeout(function() {
-              fn(scope, { $event : event });
-            }, 0);
+    angular.module('angular-gestures').directive(directiveName, ['$parse', '$log', '$timeout', 'hammerDefaultOpts', function($parse, $log, $timeout, hammerDefaultOpts) {
+        return function(scope, element, attr) {
+            var hammertime, handler;
+            attr.$observe(directiveName, function(value) {
+                var fn = $parse(value);
+                var opts = $parse(attr[directiveName + 'Opts'])(scope, {});
+                var defaultOpts = angular.copy(hammerDefaultOpts);
+
+                angular.extend(defaultOpts, opts);
+                hammertime = new Hammer(element[0], defaultOpts);
+                handler = function(event) {
+                    if (VERBOSE) {
+                      $log.debug('angular-gestures: ', eventName, event);
+                    }
+                    $timeout(function() {
+                        fn(scope, {
+                            $event: event
+                          });
+                      }, 0);
+                  };
+                hammertime.on(eventName, handler);
+              });
+            scope.$on('$destroy', function() {
+                hammertime.off(eventName, handler);
+              });
           };
-          hammertime.on(eventName, handler);
-        });
-        scope.$on('$destroy', function() {
-          hammertime.off(eventName, handler);
-        });
+      }]);
+  });
+
+angular.module('angular-gestures').provider('hammerDefaultOpts', function HammerDefaultOptsProvider() {
+    var opts = {};
+
+    this.set = function(value) {
+        opts = value;
       };
-    }]);
-});
+
+    this.$get = function() {
+        return opts;
+      };
+  });
